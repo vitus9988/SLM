0d84ff0e64c9fc4ade39766e185389d1207aed94
vitus9988 <39557109+vitus9988@users.noreply.github.com>
2025-01-30 21:33:05 +0900

    Create unsloth_finetuning.py

---
+++ finetuning/unsloth_finetuning.py
@@ -0,0 +1,96 @@
+from unsloth import FastLanguageModel
+import torch
+from trl import SFTTrainer
+from transformers import TrainingArguments
+from unsloth import is_bfloat16_supported
+from datasets import load_dataset
+
+max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
+dtype = torch.bfloat16 # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+load_in_4bit = False # Use 4bit quantization to reduce memory usage. Can be False.
+
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = "Qwen/Qwen2.5-0.5B-Instruct",
+    max_seq_length = max_seq_length,
+    dtype = dtype,
+    load_in_4bit = load_in_4bit,
+)
+
+model = FastLanguageModel.get_peft_model(
+    model,
+    r = 16,
+    target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
+                      "gate_proj", "up_proj", "down_proj",],
+    lora_alpha = 16,
+    lora_dropout = 0.5,
+    bias = "none",
+    use_gradient_checkpointing = "unsloth",
+    random_state = 3407,
+    use_rslora = False,
+    loftq_config = None,
+)
+
+alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+### Instruction:
+{}
+
+### Input:
+{}
+
+### Response:
+{}"""
+
+EOS_TOKEN = tokenizer.eos_token # Must add EOS_TOKEN
+def formatting_prompts_func(examples):
+    instructions = examples["instruction"]
+    inputs       = examples["input"]
+    outputs      = examples["output"]
+    texts = []
+    for instruction, input, output in zip(instructions, inputs, outputs):
+        text = alpaca_prompt.format(instruction, input, output) + EOS_TOKEN
+        texts.append(text)
+    return { "text" : texts, }
+
+dataset = load_dataset("yahma/alpaca-cleaned", split = "train")
+dataset = dataset.map(formatting_prompts_func, batched = True,)
+
+trainer = SFTTrainer(
+    model = model,
+    tokenizer = tokenizer,
+    train_dataset = dataset,
+    dataset_text_field = "text",
+    max_seq_length = max_seq_length,
+    dataset_num_proc = 2,
+    packing = False,
+    args = TrainingArguments(
+        per_device_train_batch_size = 4,
+        gradient_accumulation_steps = 4,
+        warmup_steps = 5,
+        num_train_epochs = 3,
+        #max_steps = 60,
+        learning_rate = 2e-4,
+        bf16 = True,
+        logging_steps = 100,
+        optim = "adamw_8bit",
+        weight_decay = 0.01,
+        lr_scheduler_type = "cosine",
+        seed = 3407,
+        output_dir = "outputs",
+        report_to = "none", # Use this for WandB etc
+    ),
+)
+
+trainer_stats = trainer.train()
+
+model.save_pretrained_merged(
+    "Qwen/Qwen2.5-0.5B-Instruct",
+    tokenizer,
+    save_method="merged_16bit"
+    )
+
+model.save_pretrained_gguf(
+    "Qwen/Qwen2.5-0.5B-Instruct-GGUF",
+    tokenizer,
+    quantization_method = "f16"
+    )
